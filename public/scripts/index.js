ff62ffe3656ad9f380a9d7178891242aa6cb6cc9
RGlendon <Rold.Glendon@gmail.com>
2020-12-10 01:05:36 +0300

    did some work

---
+++ public/scripts/index.js
@@ -0,0 +1,61 @@
+
+
+// import {registerTmpl} from "./templates/register.tmpl.js";
+
+const template = Handlebars.compile(registerTmpl);
+
+const context = {
+  modalTitle: 'Вход',
+  formName: 'enter',
+  fields: [
+    {
+      label: {
+        for: 'login',
+        text: 'Логин',
+      },
+      input: {
+        id: 'login',
+        type: 'text',
+        name: 'login',
+        className: 'input-field__input_type_email',
+        placeholder: 'Введите логин',
+      },
+      classNameError: 'input-field__error-message_login'
+    },
+    {
+      label: {
+        for: 'password',
+        text: 'Пароль',
+      },
+      input: {
+        id: 'password',
+        type: 'password',
+        name: 'password',
+        className: 'input-field__input_type_password',
+        placeholder: 'Введите пароль',
+      },
+      classNameError: 'input-field__error-message_password'
+    },
+  ],
+  buttonForm: {
+    className: 'modal__button_enter',
+    name: 'btn',
+    text: 'Авторизоваться',
+  },
+  buttonOption: {
+    className: 'modal__option_registration',
+    text: 'Нет аккаунта?',
+  }
+};
+
+const registerForm = template(context);
+
+console.log(typeof registerForm)
+
+parser = new DOMParser();
+registerFormHTMLDoc = parser.parseFromString(registerForm, "text/html");
+console.log(registerFormHTMLDoc.body.querySelector('div'))
+console.log(registerFormHTMLDoc.body.firstElementChild)
+
+document.querySelector('.modal').appendChild(registerFormHTMLDoc.body.firstElementChild);
+// document.querySelector('.modal').innerHTML = registerForm;
---
+++ public/scripts/templates/register.tmpl.js
@@ -0,0 +1,19 @@
+// export const registerTmpl = `
+const registerTmpl = `
+    <div class="modal__content modal__content-enter">
+      <h3 class="modal__title">{{modalTitle}}</h3>
+      <form class="modal__form" name="{{formName}}" novalidate>
+        {{#each fields}}
+        <div class="input-field">
+          <label for="{{this.label.for}}" class="input-field__label">{{this.label.text}}</label>
+          <input id="{{this.input.id}}" type="{{this.input.type}}" name="{{this.input.name}}" class="input-field__input {{this.input.className}}"
+                 placeholder="{{this.input.placeholder}}" required>
+          <span class="input-field__error-message {this..classNameError}}"></span>
+        </div>
+        {{/each}}
+        <button class="button modal__button {{buttonForm.className}}" name='{{buttonForm.name}}' disabled>{{buttonForm.text}}</button>
+      </form>
+      <button class="modal__option {{buttonOption.className}}">{{buttonOption.text}}</button>
+    </div>
+`;
+
