b19a499318c51685231ebca9823430e71eec4ffe
RGlendon <Rold.Glendon@gmail.com>
2021-03-26 05:23:41 +0300

    обнулил

--- public/src/Freact/interfaces.ts
+++
@@ -1,7 +0,0 @@
-export interface IObj {
-  [key: string]: any
-}
-
-export interface IProxyObj {
-  [key: string]: any
-}
--- public/src/scripts/utils/renderDom.ts
+++
@@ -1,8 +0,0 @@
-export function render(from: HTMLElement, query: string, block: Element, method = 'append') {
-  const root = from.querySelector(query);
-  if (root) {
-    if (method === 'append') root.appendChild(block);
-    if (method === 'after') root.after(block);
-  }
-  return root;
-}
