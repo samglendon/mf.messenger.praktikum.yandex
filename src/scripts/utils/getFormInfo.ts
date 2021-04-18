5c353edde405d361e61b37b829c1a587afee48d5
RGlendon <Rold.Glendon@gmail.com>
2021-04-18 14:30:00 +0300

    обнулил

--- src/scripts/utils/getFormInfo.ts
+++
@@ -1,9 +0,0 @@
-function getFormInfo(form: HTMLFormElement) {
-  const formData = new FormData(form);
-  const data = Object.fromEntries(formData.entries());
-
-  console.log(`данные из формы ${form.name}`);
-  console.dir(data);
-}
-
-export { getFormInfo };
